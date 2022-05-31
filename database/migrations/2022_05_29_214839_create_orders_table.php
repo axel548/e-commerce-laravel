0ed71794c2a1a3e932154d0b21d89bbcff3ea345
axel <axel.aatl@gmail.com>
2022-05-30 21:49:48 -0600

    Proyect and BD created

---
+++ database/migrations/2022_05_29_214839_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('id');
+            $table->bigInteger('customer_id');
+            $table->bigInteger('ammount');
+            $table->string('shipping_address');
+            $table->string('order_address');
+            $table->string('order_email');
+            $table->string('order_date');
+            $table->string('order_status');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
